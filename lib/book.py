31f94f39576e9221c5b6c72f3fa08d77b13f8806
Ian Kipchirchir Koech <iankipchirchir51@gmail.com>
2026-02-18 21:12:58 +0300

    Add Book and Coffee classes for bookstore lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+# Creating a book object (instance)
+book1 = Book("Atomic Habits", 320)
+
+# Printing attributes
+print(book1.title)
+print(book1.page_count)
+
+# Calling a method
+book1.turn_page()
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if value not in ["Small", "Medium", "Large"]:
+            print("size must be Small, Medium, or Large")
+        else:
+            self._size = value
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+
+        self.price += 1
+
+
+# testing if it works
+# Creating a coffee object
+coffee1 = Coffee("Medium", 4)
+
+# Printing attributes
+print(coffee1.size)
+print(coffee1.price)
+
+# Calling method
+coffee1.tip()
+
+# Printing updated price
+print(coffee1.price)
